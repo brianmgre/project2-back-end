a21197dbd1a3d0644a7516492e4ad931977c416f
Brian G <brian.gregorius@gmail.com>
2018-12-28 15:32:40 -0500

    server up and running and get all posts endpoint complete

--- config/routes.js
+++ config/routes.js
@@ -2,3 +2,17 @@ const bcrypt = require('bcryptjs');
 const db = require('../database/dbConfig.js');
 
 
+module.exports = server => {
+    server.get('/api/post', getPosts);
+}
+
+
+function getPosts(req, res) {
+    db('posts')
+        .then(post => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+};
