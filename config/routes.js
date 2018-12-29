b58cc437e296d85040828652c68e5900e6ed3c10
Brian G <brian.gregorius@gmail.com>
2018-12-29 12:46:54 -0500

    updated routes

--- config/routes.js
+++ config/routes.js
@@ -3,14 +3,14 @@ const db = require('../database/dbConfig.js');
 
 
 module.exports = server => {
-    server.get('/api/post', getPosts);
+    server.get('/api/posts', getPosts);
 }
 
 
 function getPosts(req, res) {
     db('posts')
         .then(post => {
-            res.status(200).json(posts)
+            res.status(200).json(post)
         })
         .catch(err => {
             res.status(500).json(err)
