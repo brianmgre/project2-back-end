ccb17a2a405b363cc4911fb97964316fd5d65d7c
Brian G <brian.gregorius@gmail.com>
2018-12-29 12:46:08 -0500

    added a posts migration

---
+++ database/migrations/20181229123954_posts.js
@@ -0,0 +1,14 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('posts', posts => {
+        posts
+            .increments();
+        posts
+            .string('postText', 155)
+            .notNullable()
+    })
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfTableExists('posts');
+};
