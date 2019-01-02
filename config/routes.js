03b3e9bde1434ca80aee3b167a03665d0724c0be
Brian G <brian.gregorius@gmail.com>
2019-01-02 17:06:49 -0500

    added delete

--- config/routes.js
+++ config/routes.js
@@ -5,6 +5,7 @@ const db = require('../database/dbConfig.js');
 module.exports = server => {
     server.get('/api/posts', getPosts);
     server.post('/api/create', newPost);
+    server.delete('/api/remove/:id', deletePost);
 }
 
 
@@ -19,9 +20,9 @@ function getPosts(req, res) {
 };
 
 function newPost(req, res) {
-    const {postText, user_id} = req.body;
-    if(!postText || !user_id){
-        res.status(422).json({message:`post required & must be a user`})
+    const { postText, user_id } = req.body;
+    if (!postText || !user_id) {
+        res.status(422).json({ message: `post required & must be a user` })
     } else {
         db('posts')
             .insert(req.body)
@@ -31,5 +32,18 @@ function newPost(req, res) {
             .catch(err => {
                 res.status(500).json(err)
             });
-    };    
+    };
+};
+
+function deletePost(req, res) {
+    const { id } = req.params;
+    db('posts')
+        .where({ id })
+        .del()
+        .then(count => {
+            res.status(200).json(count)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
 };
