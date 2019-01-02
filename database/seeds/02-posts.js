0cad8a391e0da6c9fbe2140c8ad0e3c7538f7566
Brian G <brian.gregorius@gmail.com>
2019-01-02 16:31:33 -0500

    fixed seed

--- database/seeds/02-posts.js
+++ database/seeds/02-posts.js
@@ -1,8 +1,8 @@
 
 exports.seed = function (knex) {
   return knex('posts').insert([
-    { id: 1, postText: 'this is a test post 1' },
-    { id: 2, postText: 'this is a test 2' },
-    { id: 3, postText: 'this test 3 ' }
+    { postText: 'this is a test post 1', user_id: 1  },
+    {postText: 'this is a test 2', user_id: 1  },
+    { postText: 'this test 3 ', user_id: 1 }
   ]);
 };
